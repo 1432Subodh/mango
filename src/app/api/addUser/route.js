50801c928acfcc84d6319823f5d6c307eac966c9
Subodh Ravidas <subodh14329@gmail.com>
2024-08-31 20:57:42 +0530

    s

--- src/app/api/addUser/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server"
-
-export function POST (request){
-    let payload = request.json()
-    console.log(payload)
-    return NextResponse.json({result: 'new user created'})
-}
